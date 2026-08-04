db0eb8963327526d8132e590787e9459fd1566de
9SuperBob <sirawitthamp@gmail.com>
2026-08-04 23:34:09 +0700

    Update fix 4 error screen

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/stynra-fashion-store/',
+  base: "/Vizard-fashion-store/",
+
   build: {
     cssCodeSplit: false,
     rollupOptions: {
       input: "pages-entry.js",
       output: {
-        entryFileNames: 'assets/site.js',
-        chunkFileNames: 'assets/[name].js',
+        entryFileNames: "assets/site.js",
+        chunkFileNames: "assets/[name].js",
         assetFileNames: (assetInfo) =>
-          assetInfo.name && assetInfo.name.endsWith('.css')
-            ? 'assets/site.css'
-            : 'assets/[name][extname]',
+          assetInfo.name && assetInfo.name.endsWith(".css")
+            ? "assets/site.css"
+            : "assets/[name][extname]",
       },
     },
   },
-  plugins: [react(),tailwindcss()],
-})
+
+  plugins: [react(), tailwindcss()],
+});
